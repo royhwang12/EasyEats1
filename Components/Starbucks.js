8b0116bbf44f6db28b2f376c3dc9e9e0641084b7
Devon Kim <devonrkim@yahoo.ca>
2020-06-04 03:55:01 -0700

    yessir

--- Components/Starbucks.js
+++ Components/Starbucks.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import {View, StyleSheet, Image, Text, ImageBackground } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { Rating, AirbnbRating } from 'react-native-elements';
 
 export default class MainPage extends Component {
 
@@ -13,9 +14,34 @@ export default class MainPage extends Component {
                  <ImageBackground source={require('../assets/Starbucksbackground.jpg')}
         style={styles.imagetop}>
         </ImageBackground>
-            </View>
+             </View>   
         <View>
             <Text style={styles.texts}>Starbucks</Text>
+            <Text style={styles.texts2}>1730 152 St, Surrey, BC</Text>
+          <View> 
+            <Text style={styles.texts3}>Accessibility</Text>
+          </View>
+         </View>
+            <View style={{ flexDirection: 'reverse-row'}}>
+            <Image style={styles.disability} 
+            source={require('../assets/hands.png')}/>
+            <Image style={styles.disability} 
+            source={require('../assets/noSee.png')}/>
+            <Image style={styles.disability} 
+            source={require('../assets/wheelchair.png')}/>
+            <Image style={styles.disability} 
+            source={require('../assets/autism.png')}/>
+            </View>
+            <View>
+            <Rating
+  type='star'
+  ratingColor="gold"
+  ratingBackgroundColor="white"
+  ratingCount={5}
+  imageSize={40}
+  onFinishRating={this.ratingCompleted}
+  style={{ paddingVertical: 40 }}
+/>
 
         </View>
         </View>
@@ -47,9 +73,29 @@ export default class MainPage extends Component {
             fontFamily: 'Optima-BoldItalic',
             justifyContent: 'center',
           },
-        views: {
+          texts2: {
+            fontSize: 25,
+            color: 'white',
+            fontFamily: 'Optima-Bold',
+            justifyContent: 'center',
+            paddingTop: 75
             
-        }
+        },
+        disability: {
+          height: 50,
+          width: 50,
+          alignSelf: 'center',
+          marginTop: 40
+          
+        },
+        texts3: {
+          fontSize: 20,
+          color: 'black',
+          fontFamily: 'Optima-Bold',
+          paddingTop: 20,
+         alignSelf: 'center'
+          
+      },
 
         })
 
