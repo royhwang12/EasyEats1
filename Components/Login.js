645901dc988e4591d5396226f22a0a63c0722b9d
royhwang12 <45275047+royhwang12@users.noreply.github.com>
2020-06-03 18:35:16 -0700

    hi

---
+++ Components/Login.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import {View, Text, TextInput, Stylesheet, Image, Button} from 'react-native';
+
+
+export default class LoginPage extends Component {
+    static navigationOptions = {
+        title: 'Login',
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: '',
+        }
+    }
+    render() {
+        const { onChangeText } = this;
+        const { navigate } = this.props.navigation;
+        const { username, password } = this.state;
+
+        return(
+            <View>
+
+                
+            <Text>This is the login page</Text>
+
+            
+
+            <TextInput
+            placeholder="Username"
+
+            
+            />
+            <TextInput
+            placeholder="Password"
+            />
+
+
+
+            <Button
+            onPress={() => navigate('Main')}
+            title="Login"
+            />
+        </View>
+
+        )
+
+
+    }
+
+}
---
+++ Components/Main.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+import {View, Stylesheet, Text} from 'react-native';
+
+export default class MainPage extends Component {
+
+    render() {
+
+        return(
+            <View>
+                <Text>Test run for github</Text>
+            </View>
+
+
+        )
+
+    }
+
+
+
+}
